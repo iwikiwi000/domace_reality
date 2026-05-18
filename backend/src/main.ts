f8d0a35ae790eb1636dddd959503d8d0a6d2161f
wojto <samo.vojtek25@gmail.com>
2026-05-18 14:08:15 +0200

    cors fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,8 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    //origin: 'http://localhost:5173',
+    origin: 'http://domacereality.pavojka.sk',
     credentials: true,
   });
   app.useStaticAssets(join(__dirname, '..', 'uploads'), {
